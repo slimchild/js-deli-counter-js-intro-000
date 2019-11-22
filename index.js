47637668f4aa618d350884cd2dc290ba9a50f4ab
Timothy Wyld <timwyld@gmail.com>
2019-11-22 03:13:21 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ if(katzDeliLine.length > 0){
 
 function currentLine(katzDeliLine){
   if(katzDeliLine.length === 0){
-    return "The line is currently empty";
+    return "The line is currently empty.";
   }
   let boop = "";
   for(var i = 0; i < katzDeliLine.length; i++){
